a7e777d80b1e94d5c1c83d77ee33c025372a27ce
Bona Philippe <philippe@fgx.co.za>
2021-08-29 19:05:50 +0200

    MOMO

--- src/BulkSMS/Validation/MobileOperator/MOMO.php
+++ src/BulkSMS/Validation/MobileOperator/MOMO.php
@@ -7,7 +7,7 @@ namespace Loecos\Bulksms\BulkSMS\Validation\MobileOperator;
 use Loecos\Bulksms\BulkSMS\Validation\Exceptions\ValidationException;
 use Loecos\Bulksms\BulkSMS\Validation\IPhoneNumberValidation;
 
-class OM implements IPhoneNumberValidation
+class MOMO implements IPhoneNumberValidation
 {
     /**
      * @param $number
