5d4562fcb3bf6c9ee1798e3ba86324ce6b71deac
Philippe Bona <philippe@fgx.co.za>
2022-03-14 21:31:30 +0200

    API

--- config/bulksms.php
+++ config/bulksms.php
@@ -7,7 +7,7 @@ return [
   */
     'api_version' => env('BULKSMS_API_VERSION', 'v1'),
     'sms_sender' => env('BULKSMS_SENDER', 'MOLOGAME'),
-    'base_endpoint' => env('BULKSMS_BASE_ENDPOINT', 'https:://app.lmtgroup.com/bulksms/api/%s/push'),
+    'base_endpoint' => env('BULKSMS_BASE_ENDPOINT', 'https://app.lmtgroup.com/bulksms/api/%s/push'),
     'api_key' => env('BULKSMS_API_KEY', 'YOUR API KEY'),
     'api_password' => env('BULKSMS_API_PASSWORD', 'YOUR API PASSWORD'),
     'mobile_sp' => env('BULKSMS_SP', 'OM'),
